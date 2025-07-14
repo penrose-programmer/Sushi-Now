d442de7bc8e9df824d2b5f1617bc67975ae146fd
Talhah Ahmed <talhah_ahmed@outlook.com>
2025-07-14 14:33:02 +1000

    refactor: update Vite configuration and add test setup
    - Removed Tailwind CSS plugin from Vite configuration.
    - Added setup file for Vitest to handle cleanup after tests.
    - Updated path resolution for the src directory to use fileURLToPath.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
